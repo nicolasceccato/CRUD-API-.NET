2acf72051c5726273fe283494f93384f1626904f
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-05 21:31:29 -0300

    update(project) - creating routes

---
+++ ApiCrud/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCrud.Students;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud.Data;
+
+public class AppDbContext : DbContext
+{
+    private DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Banco.sqlite");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ ApiCrud/Students/StudentsRoutes.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCrud.Students;
+
+public static class StudentsRoutes
+{
+    public static void AddStudentsRoutes(this WebApplication app)
+    {
+        app.MapGet("students", () => "Hello Students");
+    }
+}
